3016f38367e9887df7dda85cdea894f95d1a0605
hamimreja-404 <hamim.reja.mail@gmail.com>
2025-06-23 09:00:09 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base:"Madhaipur-A.R.-High-School",
+  base:"/Madhaipur-A.R.-High-School/",
   plugins: [tailwindcss(), react()],
 });
