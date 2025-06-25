8c15448fcaa0327059b3523f715a0c76d262f22f
Hamim Reja <hamim.reja.mail@gmail.com>
2025-06-25 15:52:11 +0530

    photos

--- src/Data/teachersList.js
+++ src/Data/teachersList.js
@@ -3,126 +3,126 @@ const teachersList = [
     name: "Abhik Sen",
     role: "M.A in History",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["History"]
   },
   {
     name: "Apurba Maitra",
     role: "M.Sc in Pure Mathematics",
-    image: "./images/Faculty/Aparba.jpg",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Aparba.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["Mathematics"]
   },
   {
     name: "Bapi Lohar",
     role: "English Faculty",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Chumki Debnath",
     role: "Computer Faculty",
-    image: "./images/Faculty/Chumki.jpg",
-    Avatar: "/images/Faculty/Avatar_F.jpg",
+    image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Chumki.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_F.jpg",
     subjects: ["Computer"]
   },
   {
     name: "Dulal Lakra",
     role: "Faculty",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["Education"]
   },
   {
     name: "Kaberi Choudhury",
     role: "Sanskrit Faculty",
     image: "",
-    Avatar: "/images/Faculty/Avatar_F.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_F.jpg",
     subjects: ["Sanskrit"]
   },
   {
     name: "Manoj Sarkar",
     role: "M.A in Bangali Faculty",
-    image: "./images/Faculty/Manoj.jpg",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Manoj.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["Bengali"]
   },
   {
     name: "Md Abdul Wahed",
     role: "B.A in Arabic",
-    image: "/images/Faculty/Wahed.jpg",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Wahed.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["Arabic"]
   },
   {
     name: "Md Mazed Ali",
     role: "M.A in English",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Samir Kundu",
     role: " ",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Shekhar Kr Chowdhury",
     role: "Sports Faculty",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Souvik Banerjee",
     role: " ",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Subhankar Bhattacharya",
     role: "M.SC in Biology(Zoology)",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["Life Science"]
   },
   {
     name: "Subhankar Mani",
     role: "M.A in History",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: ["History"]
   },
   {
     name: "Susanta Kumar Mandal",
     role: " ",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Satyajit Karmakar",
     role: " ",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Ratan Saha",
     role: " ",
     image: "",
-    Avatar: "/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Runu Choudhury",
     role: " ",
-    image: "/images/Faculty/Runu.jpg",
-    Avatar: "/images/Faculty/Avatar_F.jpg",
+    image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Runu.jpg",
+    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_F.jpg",
     subjects: [""]
   }
 ];
