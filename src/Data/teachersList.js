bf845f149236d8977f8d7cf9cd6d856321df862d
Hamim Reja <hamim.reja.mail@gmail.com>
2025-06-25 15:32:24 +0530

    Final Change

--- src/Data/teachersList.js
+++ src/Data/teachersList.js
@@ -3,127 +3,127 @@ const teachersList = [
     name: "Abhik Sen",
     role: "M.A in History",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["History"]
   },
   {
     name: "Apurba Maitra",
     role: "M.Sc in Pure Mathematics",
-    image: "src/images/Faculty/Aparba.jpg",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    image: "/images/Faculty/Aparba.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["Mathematics"]
   },
   {
     name: "Bapi Lohar",
     role: "English Faculty",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Chumki Debnath",
     role: "Computer Faculty",
-    image: "src/images/Faculty/Chumki.jpg",
-    Avatar: "src/images/Faculty/Avatar_F.jpg",
+    image: "/images/Faculty/Chumki.jpg",
+    Avatar: "/images/Faculty/Avatar_F.jpg",
     subjects: ["Computer"]
   },
   {
     name: "Dulal Lakra",
     role: "Faculty",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["Education"]
   },
   {
     name: "Kaberi Choudhury",
     role: "Sanskrit Faculty",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_F.jpg",
+    Avatar: "/images/Faculty/Avatar_F.jpg",
     subjects: ["Sanskrit"]
   },
   {
     name: "Manoj Sarkar",
     role: "M.A in Bangali Faculty",
-    image: "src/images/Faculty/Manoj.jpg",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    image: "/images/Faculty/Manoj.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["Bengali"]
   },
   {
     name: "Md Abdul Wahed",
     role: "B.A in Arabic",
-    image: "src/images/Faculty/Wahed.jpg",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    image: "/images/Faculty/Wahed.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["Arabic"]
   },
   {
     name: "Md Mazed Ali",
     role: "M.A in English",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Samir Kundu",
     role: " ",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["English"]
   },
   {
     name: "Shekhar Kr Chowdhury",
     role: "Sports Faculty",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Souvik Banerjee",
     role: " ",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Subhankar Bhattacharya",
     role: "M.SC in Biology(Zoology)",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["Life Science"]
   },
   {
     name: "Subhankar Mani",
     role: "M.A in History",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: ["History"]
   },
   {
     name: "Susanta Kumar Mandal",
     role: " ",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Satyajit Karmakar",
     role: " ",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Ratan Saha",
     role: " ",
     image: "",
-    Avatar: "src/images/Faculty/Avatar_M.jpg",
+    Avatar: "/images/Faculty/Avatar_M.jpg",
     subjects: [" "]
   },
   {
     name: "Runu Choudhury",
     role: " ",
-    image: "src/images/Faculty/Runu.jpg",
-    Avatar: "src/images/Faculty/Avatar_F.jpg",
-    subjects: [ ""]
+    image: "/images/Faculty/Runu.jpg",
+    Avatar: "/images/Faculty/Avatar_F.jpg",
+    subjects: [""]
   }
 ];
 
