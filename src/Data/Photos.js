68f1eaa725e87e396a1c69ca673ca534ff7a5bac
Hamim Reja <hamim.reja.mail@gmail.com>
2025-06-25 18:53:12 +0530

    Photos

--- src/Data/Photos.js
+++ src/Data/Photos.js
@@ -1,42 +1,251 @@
+
+// const Photos = [
+//     {
+//         image: "src/images/Photo/Tour_1.webp",
+//         title: "Educational Tour 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_2.webp",
+//         title: "Educational Tour 2022"
+//     },
+//     {
+//         image: "src/images/Photo/Kreta.jpg",
+//         title: "Kreta Surokkha Mela"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_4.webp",
+//         title: "Educational Tour 2023"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_5.webp",
+//         title: "Educational Tour 2020"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_7.webp",
+//         title: "Educational Tour 2023"
+//     },
+//     {
+//         image: "src/images/Photo/A_Sports (15).jpg",
+//         title: "Annual Sports 2025"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_9.webp",
+//         title: "Educational Tour 2018"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_10.webp",
+//         title: "Educational Tour 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_11.webp",
+//         title: "Educational Tour 2018"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_12.webp",
+//         title: "Educational Tour 2017"
+//     },
+//     {
+//         image: "src/images/Photo/Tour_14.webp",
+//         title: "Educational Tour 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Program_1.webp",
+//         title: "Farewell"
+//     },
+//         {
+//         image: "src/images/Photo/Program_2.webp",
+//         title: "Teacher's Day Program 2018"
+//     },
+//     {
+//         image: "src/images/Photo/Program_3.webp",
+//         title: "Republic Day Program"
+//     },
+//     {
+//         image: "src/images/Photo/Program_4.webp",
+//         title: "Teacher's Day Program 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Program_8.jpg",
+//         title: "Teacher's Day Program 2020"
+//     },
+//     {
+//         image: "src/images/Photo/Program_9.jpeg",
+//         title: "200th Birth Anniversary of Ishwar Chandra Vidyasagar "
+//     },
+//     {
+//         image: "src/images/Photo/Program_11.jpeg",
+//         title: "200th Birth Anniversary of Ishwar Chandra Vidyasagar"
+//     },
+//     {
+//         image: "src/images/Photo/Program_12.jpeg",
+//         title: "150th Birth Anniversary of Mahatma Gandhi"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_1.webp",
+//         title: "Annual Sports Event 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_2.webp",
+//         title: "Annual Sports Event 2017"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_3.webp",
+//         title: "Sports Event"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_4.webp",
+//         title: "Sports Event"
+//     },
+//     {
+//         image: "src/images/Photo/A (2).jpg",
+//         title: "Cycle Distribution to class IX"
+//     },
+//     {
+//         image: "src/images/Photo/A (1).jpg",
+//         title: "Prayer Assembly"
+//     },
+//     {
+//         image: "src/images/Photo/A (3).jpg",
+//         title: "National Yeoga Day"
+//     },
+//     {
+//         image: "src/images/Photo/A (4).jpg",
+//         title: "Teachers"
+//     },
+//     {
+//         image: "src/images/Photo/A (5).jpg",
+//         title: "Nikhil Banga Samati"
+//     },
+//     {
+//         image: "src/images/Photo/A (6).jpg",
+//         title: "National Enviroment Day"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_5.webp",
+//         title: "Annual Sports Event 2017"
+//     },
+//     {
+//         image: "src/images/Photo/Sports_6.webp",
+//         title: "Annual Sports Event 2018"
+//     },
+//     {
+//         image: "src/images/Photo/Event_1.webp",
+//         title: "Youth Parliament"
+//     },
+//     {
+//         image: "src/images/Photo/Event.webp",
+//         title: "Youth Parliament"
+//     },
+//     {
+//         image: "src/images/Photo/Event_2.webp",
+//         title: "Annual Sports Event 2019"
+//     },
+//     {
+//         image: "src/images/Photo/Event_3.jpeg",
+//         title: "Science Project"
+//     },
+//     {
+//         image: "src/images/Photo/Help.webp",
+//         title: "Relief Camp for the Flood affected people"
+//     },
+//     {
+//         image: "src/images/Photo/classroom.webp",
+//         title: "Classroom"
+//     },
+//     {
+//         image: "src/images/Photo/classroom.jpg",
+//         title: "Class V Students"
+//     },
+//     {
+//         image: "src/images/Photo/Event (6).jpg",
+//         title: "Cultural Program"
+//     },
+//     {
+//         image: "src/images/Photo/Event (5).jpg",
+//         title: "National Enviroment Day"
+//     },
+//     {
+//         image: "src/images/Photo/Event (7).jpg",
+//         title: "Saraswati Pujo"
+//     },
+//     {
+//         image: "src/images/Photo/Event (4).jpg",
+//         title: "National Anthem"
+//     },
+//     {
+//         image: "src/images/Photo/A_Sports (18).jpg",
+//         title: "Annual Sports 2025"
+//     },
+//     {
+//         image: "src/images/Photo/Event (3).jpg",
+//         title: "Cultural Program"
+//     },
+//     {
+//         image: "src/images/Photo/A_Sports (17).jpg",
+//         title: "Annual Sports 2025"
+//     },
+//     {
+//         image: "src/images/Photo/A_Sports (8).jpg",
+//         title: "Annual Sports Opening Ceremony"
+//     },
+//     {
+//         image: "src/images/Photo/A_Sports (5).jpg",
+//         title: "Annual Sports Opening Speech by president"
+//     },
+// ];
+// export default Photos
+
+
+import Tour_1 from '../images/Photo/Tour_1.webp';
+import Tour_2 from '../images/Photo/Tour_2.webp';
+import Tour_4 from '../images/Photo/Tour_4.webp';
+import Tour_5 from '../images/Photo/Tour_5.webp';
+import Tour_7 from '../images/Photo/Tour_7.webp';
+import Tour_9 from '../images/Photo/Tour_9.webp';
+import Tour_10 from '../images/Photo/Tour_10.webp';
+import Tour_11 from '../images/Photo/Tour_11.webp';
+import Tour_12 from '../images/Photo/Tour_12.webp';
+import Tour_14 from '../images/Photo/Tour_14.webp';
+
 const Photos = [
     {
-        image: "src/images/Photo/Tour_1.webp",
+        image: Tour_1,
         title: "Educational Tour 2019"
     },
     {
-        image: "src/images/Photo/Tour_2.webp",
+        image: Tour_2,
         title: "Educational Tour 2022"
     },
     {
-        image: "src/images/Photo/Kreta.jpg",
-        title: "Kreta Surokkha Mela"
+        image: Tour_4,
+        title: "Educational Tour 2022"
     },
     {
-        image: "src/images/Photo/Tour_4.webp",
+        image: Tour_5,
         title: "Educational Tour 2023"
     },
     {
-        image: "src/images/Photo/Tour_5.webp",
+        image: Tour_7,
         title: "Educational Tour 2020"
     },
     {
-        image: "src/images/Photo/Tour_7.webp",
+        image: Tour_9,
         title: "Educational Tour 2023"
     },
     {
-        image: "src/images/Photo/A_Sports (15).jpg",
-        title: "Annual Sports 2025"
+        image: Tour_10,
+        title: "Educational Tour 2023"
     },
     {
-        image: "src/images/Photo/Tour_9.webp",
+        image: Tour_11,
         title: "Educational Tour 2018"
     },
     {
-        image: "src/images/Photo/Tour_10.webp",
+        image: Tour_12,
         title: "Educational Tour 2019"
     },
     {
-        image: "src/images/Photo/Tour_11.webp",
+        image: Tour_14,
         title: "Educational Tour 2018"
     },
     {
@@ -96,28 +305,16 @@ const Photos = [
         title: "Sports Event"
     },
     {
-        image: "src/images/Photo/A (2).jpg",
-        title: "Cycle Distribution to class IX"
+        image: "src/images/Photo/Event_4.jpeg",
+        title: "Science Project"
     },
     {
-        image: "src/images/Photo/A (1).jpg",
-        title: "Prayer Assembly"
+        image: "src/images/Photo/Tour_6.webp",
+        title: "Educational Tour 2020"
     },
     {
-        image: "src/images/Photo/A (3).jpg",
-        title: "National Yeoga Day"
-    },
-    {
-        image: "src/images/Photo/A (4).jpg",
-        title: "Teachers"
-    },
-    {
-        image: "src/images/Photo/A (5).jpg",
-        title: "Nikhil Banga Samati"
-    },
-    {
-        image: "src/images/Photo/A (6).jpg",
-        title: "National Enviroment Day"
+        image: "src/images/Photo/Program_10.jpeg",
+        title: "200th Birth Anniversary of Ishwar Chandra Vidyasagar"
     },
     {
         image: "src/images/Photo/Sports_5.webp",
@@ -150,46 +347,6 @@ const Photos = [
     {
         image: "src/images/Photo/classroom.webp",
         title: "Classroom"
-    },
-    {
-        image: "src/images/Photo/classroom.jpg",
-        title: "Class V Students"
-    },
-    {
-        image: "src/images/Photo/Event (6).jpg",
-        title: "Cultural Program"
-    },
-    {
-        image: "src/images/Photo/Event (5).jpg",
-        title: "National Enviroment Day"
-    },
-    {
-        image: "src/images/Photo/Event (7).jpg",
-        title: "Saraswati Pujo"
-    },
-    {
-        image: "src/images/Photo/Event (4).jpg",
-        title: "National Anthem"
-    },
-    {
-        image: "src/images/Photo/A_Sports (18).jpg",
-        title: "Annual Sports 2025"
-    },
-    {
-        image: "src/images/Photo/Event (3).jpg",
-        title: "Cultural Program"
-    },
-    {
-        image: "src/images/Photo/A_Sports (17).jpg",
-        title: "Annual Sports 2025"
-    },
-    {
-        image: "src/images/Photo/A_Sports (8).jpg",
-        title: "Annual Sports Opening Ceremony"
-    },
-    {
-        image: "src/images/Photo/A_Sports (5).jpg",
-        title: "Annual Sports Opening Speech by president"
     },
 ];
 export default Photos
