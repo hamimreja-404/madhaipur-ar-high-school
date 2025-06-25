37c7494765072be40b5d7f7263c4989726a9f760
Hamim Reja <hamim.reja.mail@gmail.com>
2025-06-25 16:11:35 +0530

    P

--- src/Data/teachersList.js
+++ src/Data/teachersList.js
@@ -3,21 +3,21 @@ const teachersList = [
     name: "Abhik Sen",
     role: "M.A in History",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["History"]
   },
   {
     name: "Apurba Maitra",
     role: "M.Sc in Pure Mathematics",
     image: "https://drive.google.com/uc?export=view&id=111Za-o7A6Y3pZLA_xStFA4B68ySHH4sj",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Mathematics"]
   },
   {
     name: "Bapi Lohar",
     role: "English Faculty",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["English"]
   },
   {
@@ -31,98 +31,98 @@ const teachersList = [
     name: "Dulal Lakra",
     role: "Faculty",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Education"]
   },
   {
     name: "Kaberi Choudhury",
     role: "Sanskrit Faculty",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_F.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Sanskrit"]
   },
   {
     name: "Manoj Sarkar",
     role: "M.A in Bangali Faculty",
     image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Manoj.jpg",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Bengali"]
   },
   {
     name: "Md Abdul Wahed",
     role: "B.A in Arabic",
     image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Wahed.jpg",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Arabic"]
   },
   {
     name: "Md Mazed Ali",
     role: "M.A in English",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["English"]
   },
   {
     name: "Samir Kundu",
     role: " ",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["English"]
   },
   {
     name: "Shekhar Kr Chowdhury",
     role: "Sports Faculty",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [" "]
   },
   {
     name: "Souvik Banerjee",
     role: " ",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [" "]
   },
   {
     name: "Subhankar Bhattacharya",
     role: "M.SC in Biology(Zoology)",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["Life Science"]
   },
   {
     name: "Subhankar Mani",
     role: "M.A in History",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: ["History"]
   },
   {
     name: "Susanta Kumar Mandal",
     role: " ",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [" "]
   },
   {
     name: "Satyajit Karmakar",
     role: " ",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [" "]
   },
   {
     name: "Ratan Saha",
     role: " ",
     image: "",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_M.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [" "]
   },
   {
     name: "Runu Choudhury",
     role: " ",
     image: "https://hamimreja-404.github.io/MARHS/images/Faculty/Runu.jpg",
-    Avatar: "https://hamimreja-404.github.io/MARHS/images/Faculty/Avatar_F.jpg",
+    Avatar: "https://www.pexels.com/photo/man-in-red-polo-shirt-thought-a-good-idea-3779432/",
     subjects: [""]
   }
 ];
